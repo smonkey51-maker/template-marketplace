6ec70261ca89caa1cd8a136ded966efff39417d7
smonkey51-maker <nicoloforcolinprivata@gmail.com>
2026-07-30 11:32:10 +0200

    Delete MotionWrapper component
    Removed MotionWrapper component that wraps children in MotionConfig.

--- app/[lang]/catalogo/MotionWrapper.tsx
+++ app/[lang]/catalogo/MotionWrapper.tsx
@@ -1,11 +1,1 @@
-"use client";
 
-import { MotionConfig } from "framer-motion";
-
-export default function MotionWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <MotionConfig reducedMotion="never">
-      {children}
-    </MotionConfig>
-  );
-}
