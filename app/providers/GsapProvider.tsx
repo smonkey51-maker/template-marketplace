af22b8b1b25ecfbecd4b79939aae352eed07d6b1
smonkey51-maker <nicoloforcolinprivata@gmail.com>
2026-07-31 11:07:34 +0200

    Refactor GsapProvider component
    Removed global GSAP initialization and simplified return statement.

--- app/providers/GsapProvider.tsx
+++ app/providers/GsapProvider.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Registrazione dei plugin globali in ambiente browser
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+}
 
 interface GsapProviderProps {
   children: React.ReactNode;
 }
 
 export default function GsapProvider({ children }: GsapProviderProps) {
-  // Inizializzazione globale di GSAP rimossa.
-  return (
-    <>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
