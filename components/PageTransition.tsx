c9124f31d4291afaeaa6bfe2b4bc85237591c2fe
smonkey51-maker <nicoloforcolinprivata@gmail.com>
2026-07-31 10:50:37 +0200

    adjust pagetransition

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
+import React from "react";
 
 interface PageTransitionProps {
   children: React.ReactNode;
 }
 
 export default function PageTransition({ children }: PageTransitionProps) {
-  // GSAP e animazioni di transizione rimosse.
-  // Restituiamo direttamente i children per evitare blocchi del routing e dell'overflow.
-  return (
-    <div className="anim-page-enter">
-      {children}
-    </div>
-  );
+  return <div className="anim-page-enter">{children}</div>;
 }
